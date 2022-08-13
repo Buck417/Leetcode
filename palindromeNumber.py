3be83e46602b60d94bea3d7d1e2651d80e9147a0
ryan.fletcher.6 <ryan.fletcher.6@us.af.mil>
2022-08-13 12:51:11 -0600

    added good solutions

--- palindromeNumber.py
+++ palindromeNumber.py
@@ -15,5 +15,14 @@ class Solution:
                 isPalindrome = False;
                 break;
         return isPalindrome;
+
+    def goodIsPalindrome(self, x: int) -> bool:
+        if x < 0 or (x > 0 and x%10 == 0):
+            return False;
+        result = 0
+        while x > result:
+            result = result * 10 + x%10
+            x = x//10
+        return x == result or x == result // 10
 solution = Solution();
 print(solution.isPalindrome(12321));
